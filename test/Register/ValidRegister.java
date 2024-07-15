8164ce9a715fe2ea8318a8df2bdbc01d46b45934
Hala-Eldaly <121736948+Hala-Eldaly@users.noreply.github.com>
2024-07-15 14:56:37 +0300

    Add files via upload

---
+++ test/Register/ValidRegister.java
@@ -0,0 +1,27 @@
+package Register;
+import org.testng.annotations.Test;
+import org.testng.Assert;
+import Pom.Register;
+import Setup.TestBase;
+public class ValidRegister extends TestBase {
+
+    @Test(priority = 1)
+    public void registerWithValidData() throws InterruptedException {
+        Register registerPage = new Register(driver);
+        registerPage.clickRegisterLink();
+        registerPage.enterFullName("kkk");
+        registerPage.enterSSN("11100076573048");
+        registerPage.enterUsername("x_9");
+        registerPage.enterPhone("01586700650");
+       // registerPage.enterDateOfBirth("7112001");
+        registerPage.enterEmail("abd0@gmail.com");
+        registerPage.selectGender("Female");
+        registerPage.selectMaritalStatus("Single");
+        registerPage.enterPassword("12345678");
+        registerPage.confirmPassword("12345678");
+        registerPage.clickRegisterButton();
+
+        // Add assertion for successful registration (if applicable)
+        Assert.assertTrue(registerPage.verifyPhoneRequiredError());
+}
+}
